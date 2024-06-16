4c6ae0e3c8a4f38daad01610cc0cda6f11d24d60
Md. Uzzal Mia <129536126+uzzal2200@users.noreply.github.com>
2024-06-16 13:10:59 +0600

    Add files via upload

---
+++ Module 02 : Dynamic Allocation/Ternary-operator.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a=11;
+    // if(a%2==0)
+    // {
+    //     cout<<"even";
+    // }
+    // else
+    // {
+    // cout<<"odd";
+
+    // }
+    (a%2==0)? cout<<"Even" : cout<<"Odd";
+
+    return 0;
+}
